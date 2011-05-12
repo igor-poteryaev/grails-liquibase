982e5861f03cc48528cda0b983e582fc2851d93e
Igor E. Poteryaev <igor.poteryaev@gmail.com>
2011-05-12 17:42:30 +0600

    Update to grails 1.3.7
    Change-Id: I44e0ca1e5e5730591f09d778c4f23c652fe5115e

--- LiquibaseGrailsPlugin.groovy
+++ LiquibaseGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 
 class LiquibaseGrailsPlugin {
-	def version = '1.9.3.6'
+	def version = '1.9.3.7'
 	def dependsOn = [dataSource:"* > 1.0"]
 	def grailsVersion = '1.0 > *'
 
@@ -28,25 +28,4 @@ Major functionality includes:
 - Support for 10 database systems
 '''
     def documentation = 'http://www.liquibase.org/manual/grails'
-	
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}   
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)		
-	}
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional)
-	}	                                      
-	def doWithDynamicMethods = { ctx ->
-		// TODO Implement additions to web.xml (optional)
-	}	
-	def onChange = { event ->
-		// TODO Implement code that is executed when this class plugin class is changed  
-		// the event contains: event.application and event.applicationContext objects
-	}                                                                                  
-	def onApplicationChange = { event ->
-		// TODO Implement code that is executed when any class in a GrailsApplication changes
-		// the event contain: event.source, event.application and event.applicationContext objects
-	}
 }
